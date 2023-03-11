e1f3fde51bca6a8750f827cb6fc1924d77aa0361
murtazamalik <murtazamalik1319@gmail.com>
2023-03-11 21:39:19 +0530

    Added: More Than Three Parameters

--- src/generic/Main.java
+++ src/generic/Main.java
@@ -2,11 +2,14 @@ package generic;
 
 public class Main {
     public static void main(String[] args) {
-        MaxValue<Integer> maxValue1 = new MaxValue<>(5, 4, 3);
+        Integer[] intArray = {5, 7, 6, 8};
+        Float[] floatArray = {5.3F, 9.2F, 3.4F, 8.6F};
+        String[] stringArray = {"Abhi", "murtaza", "malik", "irfan"};
+        MaxValue<Integer> maxValue = new MaxValue<>(intArray);
+        maxValue.testMaximum();
+        MaxValue<Float> maxValue1 = new MaxValue<>(floatArray);
         maxValue1.testMaximum();
-        MaxValue<Float> maxValue2 = new MaxValue<>(5.2F, 8.3F, 3.2F);
+        MaxValue<String> maxValue2 = new MaxValue<>(stringArray);
         maxValue2.testMaximum();
-        MaxValue<String> maxValue3 = new MaxValue<>("Peach", "Apple", "Zach");
-        maxValue3.testMaximum();
     }
 }
